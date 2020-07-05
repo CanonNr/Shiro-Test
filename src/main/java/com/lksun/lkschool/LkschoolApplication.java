114e6bf8b22bde9f76bc4d712266e070ee107e55
CanonNr <717437709@qq.com>
2020-07-05 11:26:21 +0800

    first commit

---
+++ src/main/java/com/lksun/lkschool/LkschoolApplication.java
@@ -0,0 +1,13 @@
+package com.lksun.lkschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkschoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LkschoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lksun/lkschool/LkschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lksun.lkschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LkschoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
