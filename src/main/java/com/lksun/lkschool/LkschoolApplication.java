8d3117176effade04786f409a3fb5d47fb7e7436
CanonNr <717437709@qq.com>
2020-07-05 17:26:09 +0800

    集成MyBatis

--- src/main/java/com/lksun/lkschool/LkschoolApplication.java
+++ src/main/java/com/lksun/lkschool/LkschoolApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
+@MapperScan("com.lksun.lkschool.mapper")
 public class LkschoolApplication {
 
     public static void main(String[] args) {
