4822b311d4c50c2f4da08e72b1679f73a4ff0ec4
CanonNr <717437709@qq.com>
2020-07-05 14:58:50 +0800

    基本配置

--- src/main/java/com/lksun/lkschool/LkschoolApplication.java
+++ src/main/java/com/lksun/lkschool/LkschoolApplication.java
@@ -1,9 +1,11 @@
 package com.lksun.lkschool;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
 public class LkschoolApplication {
 
     public static void main(String[] args) {
