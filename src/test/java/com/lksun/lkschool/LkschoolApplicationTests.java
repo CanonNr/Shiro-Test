b57552da522bfa4ad6f46cf7fe6bf95341e179f2
CanonNr <717437709@qq.com>
2020-07-12 13:20:33 +0800

    JWT测试用例

--- src/test/java/com/lksun/lkschool/LkschoolApplicationTests.java
+++ src/test/java/com/lksun/lkschool/LkschoolApplicationTests.java
@@ -1,13 +1,30 @@
 package com.lksun.lkschool;
 
+import com.lksun.lkschool.common.utils.JwtTokenUtil;
+import com.lksun.lkschool.entity.Administrators;
+import com.lksun.lkschool.service.AdministratorsService;
+import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LkschoolApplicationTests {
 
+    @Autowired
+    AdministratorsService administratorsService;
+    @Autowired
+    JwtTokenUtil jwtTokenUtil;
+
     @Test
-    void contextLoads() {
+    void create() {
+        Administrators admin = administratorsService.getByUsername("admin");
+        String jwt = jwtTokenUtil.createJWT(admin);
+        System.out.println(jwt);
+
+
+        Claims claims = jwtTokenUtil.parseJWT(jwt);
+        System.out.println(claims);
     }
 
 }
