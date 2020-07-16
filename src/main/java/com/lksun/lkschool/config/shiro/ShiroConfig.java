34e480e4c6862d363c0d6785916d1d43e4b824e6
CanonNr <717437709@qq.com>
2020-07-16 22:36:47 +0800

    如果实现权限控制先配置AOP

--- src/main/java/com/lksun/lkschool/config/shiro/ShiroConfig.java
+++ src/main/java/com/lksun/lkschool/config/shiro/ShiroConfig.java
@@ -3,8 +3,10 @@ package com.lksun.lkschool.config.shiro;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.session.mgt.SessionManager;
 import org.apache.shiro.session.mgt.eis.EnterpriseCacheSessionDAO;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -77,6 +79,20 @@ public class ShiroConfig {
     @Bean(name = "userRealm")
     public UserRealm getRealm(){
         return new UserRealm();
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+        advisorAutoProxyCreator.setProxyTargetClass(true);
+        return advisorAutoProxyCreator;
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor() {
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        authorizationAttributeSourceAdvisor.setSecurityManager(defaultSecurityManager);
+        return authorizationAttributeSourceAdvisor;
     }
 
 }
--- src/main/java/com/lksun/lkschool/controller/AccountController.java
+++ src/main/java/com/lksun/lkschool/controller/AccountController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/account")
-public class UserController {
+public class AccountController {
 
     @Autowired
     AdministratorsService administratorsService;
